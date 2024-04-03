6b4645674b1f89e52d6686ba36359e25c50d56d1
Yonle <yonle@lecturify.net>
2024-04-03 18:40:37 +0700

    try to load audio first, change mouseover as pointerover
    Signed-off-by: Yonle <yonle@lecturify.net>

---
+++ audio.js
@@ -0,0 +1,23 @@
+let au = {
+  sh: "shotgun.mp3",
+  bm: "explosion.mp3",
+  ht: "hit.mp3",
+  sc: "toms-screams.mp3",
+  pw: "powerup.mp3",
+  sus: "suspense.mp3"
+}
+
+async function loadAud() {
+  for (const n of Object.keys(au)) {
+    const audioBlob = await fetch(au[n]).then(_ => _.blob());
+    let b = URL.createObjectURL(audioBlob);
+
+    au[n] = _ => {
+      let a = new Audio();
+      a.src = b;
+      a.play();
+    }
+  }
+
+  return true;
+}
